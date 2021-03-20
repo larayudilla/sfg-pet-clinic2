30585c0ff7d556b6f23b4513ddef9b7bc8769ddb
larayudilla <larayudilla@gmail.com>
2021-03-21 03:52:33 +0700

    Adding Vets List and update controller. Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Bootstrap/DataLoader.java
@@ -40,14 +40,14 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setId(1L);
         vet1.setFirstName("Sam");
-        vet1.setFirstName("Axe");
+        vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setId(2L);
         vet2.setFirstName("Jessica");
-        vet2.setFirstName("Porter");
+        vet2.setLastName("Porter");
 
         vetService.save(vet2);
 
